acacfa6f503bf17fdafc587a8d5d6803284d81fd
sairamkotipalli <ramkotipalli@Rams-MacBook-Air.local>
2025-12-09 16:31:40 +0530

    auth anfd db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
-import { start } from 'repl';
+import { serve } from "inngest/express";
+import {inngest} from './lib/inngest.js';
 
 const app = express();
 
 const __dirname = path.resolve();
+
+//middlewares
+app.use(express.json());
+//Crendentials: true Meaning?? => Server allow a browser to include cookies on request
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+
+app.use("api/inngest", serve({client:inngest, functions}));
+
 
 app.get("/health", (req, res) => {
     res.status(200).json({ msg: "api is running success" });
