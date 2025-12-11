ac97087f72422aecbfe809fd99d8295bb8ab6438
sairamkotipalli <ramkotipalli@Rams-MacBook-Air.local>
2025-12-11 13:46:14 +0530

    update3

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,7 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
-import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "talent-iq" });
 
@@ -22,11 +21,6 @@ const syncUser = inngest.createFunction(
 
     await User.create(newUser);
 
-    await upsertStreamUser({
-      id: newUser.clerkId.toString(),
-      name: newUser.name,
-      image: newUser.profileImage,
-    });
   }
 );
 
