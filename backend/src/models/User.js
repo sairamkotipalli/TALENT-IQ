b308c017089d8db3f900b07f1041678435129e6f
sairamkotipalli <ramkotipalli@Rams-MacBook-Air.local>
2025-12-11 13:34:38 +0530

    update1

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,29 +1,29 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        profileImage: {
-            type: String,
-            default: "",
-        },
-        clerkID:{
-            type: String,
-            required: true,
-            unique: true,
-        },
-    },{timestamps: true} //createdAt, updatedAt (like member Since 2022)
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true } // createdAt, updatedAt
 );
 
-
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
 export default User;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,50 +1,50 @@
-import express from 'express';
-import path from 'path';
-import cors from 'cors';
-import { ENV } from './lib/env.js';
-import { connectDB } from './lib/db.js';
+import express from "express";
+import path from "path";
+import cors from "cors";
 import { serve } from "inngest/express";
-import {inngest,functions} from './lib/inngest.js';
+import { clerkMiddleware } from "@clerk/express";
+
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
+
+import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
 const __dirname = path.resolve();
 
-//middlewares
+// middleware
 app.use(express.json());
-//Crendentials: true Meaning?? => Server allow a browser to include cookies on request
-app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
-app.use('/api/inngest', serve({ client: inngest, functions}));
-
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
 
 app.get("/health", (req, res) => {
-    res.status(200).json({ msg: "api is running success" });
+  res.status(200).json({ msg: "api is up and running" });
 });
 
-app.get("/books", (req, res) => {
-    res.status(200).json({ msg: "this is books end point" });
-});
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//make our app ready for deployment
-if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-    app.get("{*/any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist","index.html"));
-    });
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-
-const startServer = async() => {
-    try {
-        await connectDB();
-        app.listen(ENV.PORT,() => {
-            console.log("Server is running on port:",ENV.PORT);
-        });
-    } catch (error) {
-        console.error("💥 Error Starting the server", error);
-    }
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
+  }
 };
 
 startServer();
